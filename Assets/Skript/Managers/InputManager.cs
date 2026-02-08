8fcdd3d29a19c063853ebf7f233d02e14555e79a
Max <buckel20161@hs-ansbach.de>
2026-02-08 01:43:15 +0100

    Added an Input Manager to generalize all Inputs into one Script
    Currently only supports dialog. Future iterations could move the Item Interaction Input, Collectable Input and Reach Input here

---
+++ Assets/Skript/Managers/InputManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager instance;
+    private bool submitPressed = false;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("There is more than one InputManager in the scene");
+        }
+        instance = this;
+    }
+
+    public static InputManager GetInstance()
+    {
+        return instance;
+    }
+
+    
+    public void SubmitPressed(InputAction.CallbackContext context) // checking if we're pressing the space button
+    {
+        if (context.performed)
+        {
+            submitPressed = true;
+        }
+        else if (context.canceled)
+        {
+            submitPressed = false;
+        }
+    }
+
+    public bool SubmitPressed()
+    {
+        bool result = submitPressed;
+        submitPressed = false;
+        return result;
+    }
+    
+   
+}
